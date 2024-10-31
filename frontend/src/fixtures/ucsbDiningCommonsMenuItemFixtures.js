5c29f39996f71a7c14ea76adece30409c4513df7
Caleb Stahl <calebstahl@umail.ucsb.edu>
2024-10-30 19:18:40 -0700

    adding fixtures

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,28 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+    oneDCMI: {
+        id: 1,
+        diningCommonsCode: "17",
+        name: "Bacon",
+        station: "Breakfast",
+    },
+    threeDCMI: [
+        {
+            id: 1,
+            diningCommonsCode: "12",
+            name: "French Toast",
+            station: "Breakfast",
+        }, {
+            id: 2,
+            diningCommonsCode: "19",
+            name: "Eggs",
+            station: "Breakfast",
+        }, {
+            id: 3,
+            diningCommonsCode: "11",
+            name: "Oatmeal",
+            station: "Breakfast",
+        },
+    ],  
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
