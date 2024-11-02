a1612a7439e1491f13d110e2500532f5bc294ed2
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-02 16:21:06 -0700

    table and stories created

--- frontend/src/fixtures/menuItemReviewFixtures.js
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -1,16 +1,16 @@
 const menuItemReviewFixtures = {
   oneReview: {
     id: 0,
-    itemI: 0,
+    itemId: 0,
     reviewerEmail: "obenedek@ucsb.edu",
     stars: 3,
     dateReviewed: "2022-01-02T12:00:00",
     comments: "good",
   },
-  threeDates: [
+  threeReviews: [
     {
       id: 0,
-      itemI: 0,
+      itemId: 0,
       reviewerEmail: "obenedek@ucsb.edu",
       stars: 3,
       dateReviewed: "2022-01-02T12:00:00",
@@ -18,7 +18,7 @@ const menuItemReviewFixtures = {
     },
     {
       id: 1,
-      itemI: 4,
+      itemId: 4,
       reviewerEmail: "calebstahl@ucsb.edu",
       stars: 4,
       dateReviewed: "2022-01-02T12:00:00",
@@ -26,7 +26,7 @@ const menuItemReviewFixtures = {
     },
     {
       id: 2,
-      itemI: 2,
+      itemId: 2,
       reviewerEmail: "randomstudent@ucsb.edu",
       stars: 5,
       dateReviewed: "2022-01-02T12:00:00",
