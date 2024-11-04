cb7333b68ad0d69244565ae486b1e4112a3c04b4
Amber Wang <ywang116@ucsb.edu>
2024-11-04 10:56:22 -0800

    Revert "fix faild #35 issue"
    This reverts commit de2007736ac72d29d9512e82cf9cde9f125968aa.
    
    "revert modifing all 12 files"

--- frontend/src/fixtures/ucsbOrganizationFixtures.js
+++ frontend/src/fixtures/ucsbOrganizationFixtures.js
@@ -1,32 +1,32 @@
 const ucsbOrganizationFixtures = {
-  oneOrganization: [
-    {
-      orgCode: "DSC",
-      orgTranslationShort: "Data Science Club",
-      orgTranslation: "UCSB Data Science Club",
-      inactive: true,
-    },
-  ],
+    oneOrganization: [
+      {
+        orgCode: "DSC",
+        orgTranslationShort: "Data Science Club",
+        orgTranslation: "UCSB Data Science Club",
+        inactive: true,
+      },
+    ],
 
-  threeOrganizations: [
-    {
-      orgCode: "CSB",
-      orgTranslationShort: "Coders SB",
-      orgTranslation: "Coders SB Prgramming Club",
-      inactive: false,
-    },
-    {
-      orgCode: "DSP",
-      orgTranslationShort: "Delta Sigma Pi",
-      orgTranslation: "Delta Sigma Pi Business Fraternity",
-      inactive: false,
-    },
-    {
-      orgCode: "WIB",
-      orgTranslationShort: "Women in Business",
-      orgTranslation: "UCSB Women in Business community",
-      inactive: true,
-    },
-  ],
-};
-export { ucsbOrganizationFixtures };
+    threeOrganizations: [
+      {
+        orgCode: "CSB",
+        orgTranslationShort: "Coders SB",
+        orgTranslation: "Coders SB Prgramming Club",
+        inactive: false,
+      },
+      {
+        orgCode: "DSP",
+        orgTranslationShort: "Delta Sigma Pi",
+        orgTranslation: "Delta Sigma Pi Business Fraternity",
+        inactive: false,
+      },
+      {
+        orgCode: "WIB",
+        orgTranslationShort: "Women in Business",
+        orgTranslation: "UCSB Women in Business community",
+        inactive: true,
+      },
+    ],
+  };
+  export { ucsbOrganizationFixtures };
