3b309657eed8f556cd6406f33005ea7f1751a715
Christy Yu <christyyu@ucsb.edu>
2024-11-05 00:26:38 -0800

    added fixtures

---
+++ frontend/src/fixtures/articlesFixtures.js
@@ -0,0 +1,45 @@
+const articlesFixtures = {
+  oneArticle: [
+    {
+      id: 1,
+      name: "The Best Restaurant in Isla Vista",
+      url: "https://www.yelp.com/biz/woodstocks-pizza-isla-vista",
+      explanation: "Woodstock's Pizza is the best restaurant in Isla Vista.",
+      email: "reviews@yelp.com",
+      dateAdded: "2022-03-11T00:00:00",
+    },
+  ],
+
+  threeRestaurants: [
+    {
+      id: 2,
+      name: "The Best Restaurant in Goleta",
+      url: "https://www.yelp.com/biz/frog-bar-and-grill-goleta",
+      explanation: "Frog Bar and Grill is the best restaurant in Goleta.",
+      email: "reviews@yelp.com",
+      dateAdded: "2022-03-12T00:00:00",
+    },
+
+    {
+      id: 3,
+      name: "The Best Restaurant in Santa Barbara",
+      url: "https://www.yelp.com/biz/la-super-rica-taqueria-santa-barbara",
+      explanation:
+        "La Super Rica Taqueria is the best restaurant in Santa Barbara.",
+      email: "reviews@yelp.com",
+      dateAdded: "2022-03-13T00:00:00",
+    },
+
+    {
+      id: 4,
+      name: "The Best Restaurant in Montecito",
+      url: "https://www.yelp.com/biz/jeannines-restaurant-and-bakery-montecito",
+      explanation:
+        "Jeannine's Restaurant and Bakery is the best restaurant in Montecito.",
+      email: "reviews@yelp.com",
+      dateAdded: "2022-03-14T00:00:00",
+    },
+  ],
+};
+
+export { articlesFixtures };
