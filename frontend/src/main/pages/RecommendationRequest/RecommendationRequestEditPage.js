4ef017527d3c86574a6686ed095a4c42cb8f15da
Mike Petrus <Petrus@ucsb.edu>
2024-11-06 20:15:28 -0800

    function name change

--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestEditPage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestEditPage.js
@@ -1,6 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function PlaceholderEditPage() {
+export default function RecommendationRequestEditPage() {
   // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
--- frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
+++ frontend/src/main/pages/RecommendationRequest/RecommendationRequestIndexPage.js
@@ -1,6 +1,6 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
 
-export default function PlaceholderIndexPage() {
+export default function RecommendationRequestIndexPage() {
   // Stryker disable all : placeholder for future implementation
   return (
     <BasicLayout>
