b38f5bde5a160ef437df10076f1f4277f8cc0054
obenedek20 <obenedek20@users.noreply.github.com>
2024-11-07 13:14:17 -0800

    fixing merge errors

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -83,6 +83,7 @@ export default function AppNavbar({
                   </Nav.Link>
                   <Nav.Link as={Link} to="/helprequests">
                     Help Requests
+                  </Nav.Link>
                   <Nav.Link as={Link} to="/ucsborganization">
                     UCSBOrganization
                   </Nav.Link>
