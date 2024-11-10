d5dae0e6f7da6fa03e4471212681d8c91039279c
Amber Wang <ywang116@ucsb.edu>
2024-11-10 17:18:32 -0600

    Revert "editPage-UCSBOrgs"
    This reverts commit 5c42be5a1510f9d44cf7a57320bf10a58ae097a1.

--- frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.js
+++ frontend/src/tests/pages/UCSBOrganization/UCSBOrganizationEditPage.test.js
@@ -1,191 +1,43 @@
-import { fireEvent, render, waitFor, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { MemoryRouter } from "react-router-dom";
-import UCSBOrganizationEditPage from "main/pages/UCSBOrganization/UCSBOrganizationEditPage";
 
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
 
-import mockConsole from "jest-mock-console";
-
-const mockToast = jest.fn();
-jest.mock("react-toastify", () => {
-  const originalModule = jest.requireActual("react-toastify");
-  return {
-    __esModule: true,
-    ...originalModule,
-    toast: (x) => mockToast(x),
-  };
-});
-
-const mockNavigate = jest.fn();
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom");
-  return {
-    __esModule: true,
-    ...originalModule,
-    useParams: () => ({
-      id: "CSB",
-    }),
-    Navigate: (x) => {
-      mockNavigate(x);
-      return null;
-    },
-  };
-});
-
 describe("UCSBOrganizationEditPage tests", () => {
   const axiosMock = new AxiosMockAdapter(axios);
 
-  describe("when the backend doesn't return data", () => {
-    beforeEach(() => {
-      axiosMock.reset();
-      axiosMock.resetHistory();
-      axiosMock
-        .onGet("/api/currentUser")
-        .reply(200, apiCurrentUserFixtures.userOnly);
-      axiosMock
-        .onGet("/api/systemInfo")
-        .reply(200, systemInfoFixtures.showingNeither);
-      axiosMock
-        .onGet("/api/ucsborganization", { params: { orgCode: "CSB" } })
-        .timeout();
-    });
+  const setupUserOnly = () => {
+    axiosMock.reset();
+    axiosMock.resetHistory();
+    axiosMock
+      .onGet("/api/currentUser")
+      .reply(200, apiCurrentUserFixtures.userOnly);
+    axiosMock
+      .onGet("/api/systemInfo")
+      .reply(200, systemInfoFixtures.showingNeither);
+  };
 
-    const queryClient = new QueryClient();
-    test("renders header but form is not present", async () => {
-      const restoreConsole = mockConsole();
+  const queryClient = new QueryClient();
+  test("Renders expected content", async () => {
+    // arrange
 
-      render(
-        <QueryClientProvider client={queryClient}>
-          <MemoryRouter>
-            <UCSBOrganizationEditPage />
-          </MemoryRouter>
-        </QueryClientProvider>,
-      );
-      await screen.findByText("Edit UCSB Organization");
-      expect(
-        screen.queryByTestId("UCSBOrganizationForm-orgCode"),
-      ).not.toBeInTheDocument();
-      restoreConsole();
-    });
-  });
+    setupUserOnly();
 
-  describe("tests where backend is working normally", () => {
-    beforeEach(() => {
-      axiosMock.reset();
-      axiosMock.resetHistory();
-      axiosMock
-        .onGet("/api/currentUser")
-        .reply(200, apiCurrentUserFixtures.userOnly);
-      axiosMock
-        .onGet("/api/systemInfo")
-        .reply(200, systemInfoFixtures.showingNeither);
-      axiosMock
-        .onGet("/api/ucsborganization", { params: { orgCode: "CSB" } })
-        .reply(200, {
-          orgCode: "CSB",
-          orgTranslationShort: "Coders SB",
-          orgTranslation: "Coders SB Prgramming Club",
-          inactive: false,
-        });
-      axiosMock.onPut("/api/ucsborganization").reply(200, {
-        orgCode: "CSB",
-        orgTranslationShort: "Updated Coders SB",
-        orgTranslation: "Updated Coders SB Prgramming Club",
-        inactive: true,
-      });
-    });
+    // act
+    render(
+      <QueryClientProvider client={queryClient}>
+        <MemoryRouter>
+          <UCSBOrganizationEditPage />
+        </MemoryRouter>
+      </QueryClientProvider>,
+    );
 
-    const queryClient = new QueryClient();
-    test("renders without crashing", async () => {
-      render(
-        <QueryClientProvider client={queryClient}>
-          <MemoryRouter>
-            <UCSBOrganizationEditPage />
-          </MemoryRouter>
-        </QueryClientProvider>,
-      );
-
-      await screen.findByTestId("UCSBOrganizationForm-orgCode");
-    });
-
-    test("Is populated with the data provided", async () => {
-      render(
-        <QueryClientProvider client={queryClient}>
-          <MemoryRouter>
-            <UCSBOrganizationEditPage />
-          </MemoryRouter>
-        </QueryClientProvider>,
-      );
-
-      await screen.findByTestId("UCSBOrganizationForm-orgCode");
-
-      const orgCodeField = screen.getByTestId("UCSBOrganizationForm-orgCode");
-      const orgTranslationShortField = screen.getByTestId(
-        "UCSBOrganizationForm-orgTranslationShort",
-      );
-      const orgTranslationField = screen.getByTestId(
-        "UCSBOrganizationForm-orgTranslation",
-      );
-      const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
-      const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
-
-      expect(orgCodeField).toHaveValue("CSB");
-      expect(orgTranslationShortField).toHaveValue("Coders SB");
-      expect(orgTranslationField).toHaveValue("Coders SB Prgramming Club");
-      expect(inactiveField).toHaveValue("false");
-      expect(submitButton).toBeInTheDocument();
-    });
-
-    test("Changes when you click Update", async () => {
-      render(
-        <QueryClientProvider client={queryClient}>
-          <MemoryRouter>
-            <UCSBOrganizationEditPage />
-          </MemoryRouter>
-        </QueryClientProvider>,
-      );
-
-      await screen.findByTestId("UCSBOrganizationForm-orgCode");
-
-      const orgTranslationShortField = screen.getByTestId(
-        "UCSBOrganizationForm-orgTranslationShort",
-      );
-      const orgTranslationField = screen.getByTestId(
-        "UCSBOrganizationForm-orgTranslation",
-      );
-      const inactiveField = screen.getByTestId("UCSBOrganizationForm-inactive");
-      const submitButton = screen.getByTestId("UCSBOrganizationForm-submit");
-
-      fireEvent.change(orgTranslationShortField, {
-        target: { value: "Updated Coders SB" },
-      });
-      fireEvent.change(orgTranslationField, {
-        target: { value: "Updated Coders SB Prgramming Club" },
-      });
-      fireEvent.change(inactiveField, { target: { value: "true" } });
-
-      fireEvent.click(submitButton);
-
-      await waitFor(() => expect(mockToast).toBeCalled());
-      expect(mockToast).toHaveBeenCalledWith(
-        "UCSB Organization Updated - orgCode: CSB",
-      );
-      expect(mockNavigate).toHaveBeenCalledWith({ to: "/ucsborganization" });
-
-      expect(axiosMock.history.put.length).toBe(1); // times called
-      expect(axiosMock.history.put[0].params).toEqual({ orgCode: "CSB" });
-      expect(axiosMock.history.put[0].data).toBe(
-        JSON.stringify({
-          orgCode: "CSB",
-          orgTranslationShort: "Updated Coders SB",
-          orgTranslation: "Updated Coders SB Prgramming Club",
-          inactive: "true",
-        }),
-      ); // posted object
-    });
+    // assert
+    await screen.findByText("Edit page not yet implemented");
   });
 });
